6a0e363cf9cfddcaee5c4f884ecee4aef8c0c2f0
Venkat Jawahar Reddy Yerrabathini <yvjr95@gmail.com>
2022-11-01 18:22:46 -0700

    Completed the Dice Rolling project with number and also added the new dices images

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.dice_roller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // This will helps to connects between the layout and main activity
+        var rollbutton: Button = findViewById(R.id.roll_button)
+        //rollbutton.text = "Let's Roll"  Modified the text on button
+        rollbutton.setOnClickListener {
+            // Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show() this helps display small popup on screen
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        var resulttext: TextView = findViewById(R.id.result_text)
+        var randomdice = Random().nextInt(6) + 1
+        resulttext.text = randomdice.toString()
     }
 }
